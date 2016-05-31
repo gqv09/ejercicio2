7687d74ba705510c5a7a4c0116a1c430c97f2826
jose <josearfclass@gmail.com>
2016-05-31 18:19:46 -0400

    jose_commit

---
+++ aritmetica.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class aritmetica here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class aritmetica
+{
+
+    public aritmetica()
+    {
+    }
+    
+    //JOSE
+    public int sumar(int a,int b)
+    {
+        return a+b;
+    }
+    
+    //MARCO
+
+	public int 
+    
+    //RODRIGO
+    
+    //GREDORIO
+    
+    
+    
+}
